17c3c424969e7f2289bb9bd36fb9f2d69b1dec3f
Bernhard K. Weisshuhn <bkw@codingforce.com>
2014-10-11 21:16:25 +0200

    use empty users data bag for basic testing

--- spec/recipes/server_spec.rb
+++ spec/recipes/server_spec.rb
@@ -19,32 +19,20 @@ require 'spec_helper'
 
 describe 'ssh-hardening::server' do
 
+  before(:each) do
+    ChefSpec::Server.create_data_bag('users', 'someuser' => { id: 'someuser' })
+  end
+
   # converge
   cached(:chef_run) do
     ChefSpec::Runner.new.converge(described_recipe)
   end
 
   it 'installs openssh-server' do
-    ChefSpec::Server.create_data_bag(
-      'users',
-      'root' => {
-        'id' => 'root',
-        'ssh_rootkeys' => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDSBlQNUoWqzhOcsyJczKWtA8b2iSKYPl+KcuHlFmrroA6OVu5LrwS3PTy5Ff4d7B4KW0A85StqwtGv9GGnTdI2urCl4eS4PUJifV1pTBh9BWMyStnZoXEy+com5YLcmMBcuDZAwfI9OnPAbfozA1/WB5CP+eXeVuniLoL90U2+xc3KjRyfvCSnK2e6v5K7lYbvmGKUBskG0EYtpX6i9/oiCvaVFLKE5s/Xk9NGjuz0QQPylFFZ9npqKzqmfotEAMbDQxMrNV2ML//9ezVVFWBjI+zyty5QYmhr/bnY7vSqNpGw+czSTGvu3w0z9i2hnVle2WeW0TCkLUNqcu32LM7BCDpXhcsWAGL/ARp0ls8YfPk4nx6mb2Kz877AzhsX0QsiGFtgbcKPMHwdkfeHkBpcEI0X3H9pfA5V9mIqrIZ3bfcV7U0fuwqgAzvfoZYBbBkYiwOhJleW4zg7vIRbFCr5hH7zJmu0fId8ceCnPqX5tXN6qupf/FODaSox/PV/RiMrBHFV2U4/hy8DRKWfifccH9YwwXvl6mLkwz0kbRc6fOf+zdkUS31ip9trUQNjjg2AweNKpG3hN8PAGeGIiEjHlrhRJ4jKJvc4LxupDkuZlBgiNBUxJwf4EYhDC81vqfKgSil9GfTncEW9I/P5rE1qRFtcfdwJFrU4i8/kHN2u8Q== root@bt'
-      }
-    )
-
     expect(chef_run).to install_package('openssh-server')
   end
 
   it 'creates /etc/ssh/ssh_config' do
-    ChefSpec::Server.create_data_bag(
-      'users',
-      'root' => {
-        'id' => 'root',
-        'ssh_rootkeys' => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDSBlQNUoWqzhOcsyJczKWtA8b2iSKYPl+KcuHlFmrroA6OVu5LrwS3PTy5Ff4d7B4KW0A85StqwtGv9GGnTdI2urCl4eS4PUJifV1pTBh9BWMyStnZoXEy+com5YLcmMBcuDZAwfI9OnPAbfozA1/WB5CP+eXeVuniLoL90U2+xc3KjRyfvCSnK2e6v5K7lYbvmGKUBskG0EYtpX6i9/oiCvaVFLKE5s/Xk9NGjuz0QQPylFFZ9npqKzqmfotEAMbDQxMrNV2ML//9ezVVFWBjI+zyty5QYmhr/bnY7vSqNpGw+czSTGvu3w0z9i2hnVle2WeW0TCkLUNqcu32LM7BCDpXhcsWAGL/ARp0ls8YfPk4nx6mb2Kz877AzhsX0QsiGFtgbcKPMHwdkfeHkBpcEI0X3H9pfA5V9mIqrIZ3bfcV7U0fuwqgAzvfoZYBbBkYiwOhJleW4zg7vIRbFCr5hH7zJmu0fId8ceCnPqX5tXN6qupf/FODaSox/PV/RiMrBHFV2U4/hy8DRKWfifccH9YwwXvl6mLkwz0kbRc6fOf+zdkUS31ip9trUQNjjg2AweNKpG3hN8PAGeGIiEjHlrhRJ4jKJvc4LxupDkuZlBgiNBUxJwf4EYhDC81vqfKgSil9GfTncEW9I/P5rE1qRFtcfdwJFrU4i8/kHN2u8Q== root@bt'
-      }
-    )
-
     expect(chef_run).to create_template('/etc/ssh/sshd_config').with(
       user:   'root',
       group:  'root',
