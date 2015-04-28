11929d4f3488e4ba0d9bd1a9eccba51f93fbe107
Christoph Hartmann <chris@lollyrock.com>
2015-04-28 14:14:21 +0200

    improve description

--- metadata.rb
+++ metadata.rb
@@ -19,7 +19,7 @@ name             "ssh-hardening"
 maintainer       "Dominik Richter"
 maintainer_email "dominik.richter@googlemail.com"
 license          "Apache 2.0"
-description      "Installs/Configures ssh"
+description      "This cookbook installs and provides secure ssh and sshd configurations."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.1.0"
 
