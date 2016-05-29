78214a84f15410a16c87420bb99b9addfacda8b3
Christoph Hartmann <chris@lollyrock.com>
2016-05-29 20:48:24 +0200

    1.2.0

--- metadata.rb
+++ metadata.rb
@@ -21,7 +21,7 @@ maintainer_email "dominik.richter@googlemail.com"
 license          "Apache 2.0"
 description      "This cookbook installs and provides secure ssh and sshd configurations."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.2.0"
 
 recipe 'ssh-hardening::default', 'installs and configures ssh client and server'
 recipe 'ssh-hardening::client', 'install and apply security hardening for ssh client'
