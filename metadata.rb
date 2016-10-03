4a70b45214aef947b03ce0e7c3b41d9aca1dab9f
Grant Ridder <shortdudey123@gmail.com>
2016-10-03 12:47:34 -0700

    Add metadata.rb to Rubocop checking

--- metadata.rb
+++ metadata.rb
@@ -15,13 +15,13 @@
 # limitations under the License.
 #
 
-name             "ssh-hardening"
-maintainer       "Dominik Richter"
-maintainer_email "dominik.richter@googlemail.com"
-license          "Apache 2.0"
-description      "This cookbook installs and provides secure ssh and sshd configurations."
+name             'ssh-hardening'
+maintainer       'Dominik Richter'
+maintainer_email 'dominik.richter@googlemail.com'
+license          'Apache 2.0'
+description      'This cookbook installs and provides secure ssh and sshd configurations.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.1"
+version          '1.2.1'
 
 recipe 'ssh-hardening::default', 'installs and configures ssh client and server'
 recipe 'ssh-hardening::client', 'install and apply security hardening for ssh client'
