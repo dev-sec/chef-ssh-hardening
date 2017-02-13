56972dd8d3932e721c664d7687d20f3d7769662d
Artem Sidorenko <artem@posteo.de>
2017-02-13 20:25:07 +0100

    Removal of apt/yum cookbooks from tests
    They produce broken builds and we do not need them anymore:
    https://travis-ci.org/dev-sec/chef-ssh-hardening/jobs/201236096
    
    In the same time we need to run apt-get update at the start of chef-run
    as ubuntu does not have any apt repo information. This is the reason
    for introduction of test cookbook.

---
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -0,0 +1,9 @@
+name             'test'
+maintainer       'Dominik Richter'
+maintainer_email 'dominik.richter@googlemail.com'
+license          'Apache 2.0'
+description      'This cookbook is used for testing purposes'
+long_description 'This cookbook is used for testing purposes'
+version          '0.0.1'
+
+depends 'compat_resource', '>= 12.16.3'
